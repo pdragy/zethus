7798b3e3aa7b6f100e0ed1f8d046beb27684799d
Paul Draghicescu <pdragy@gmail.com>
2025-07-15 22:44:51 -0700

    works with node 8

--- webpack.app.dev.js
+++ webpack.app.dev.js
@@ -20,6 +20,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
